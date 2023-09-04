f9ec0f8d58fa9cbbf50508de1e9ae3ad2d90f1a8
abdulRehamanS <abdulrehaman627@gmail.com>
2023-09-04 22:23:45 +0530

    Template Driven Form Development

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'FormsDevelopment';
+  // loginUser(item: any) {
+  //   console.warn(item);
+  // }
+
+  loginValid(item: any) {
+    console.warn(item);
+  }
 }
